c8f5ab986c8bbd52b5d95471a8e8b3a08510cce3
Ga-bee <116361973+Ga-bee@users.noreply.github.com>
2022-12-12 12:52:16 -0300

    Update streamlit_app.py
    Adicionei parte de cardapio

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+strealit.header('Breakfest menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('Kale, Spinach & Rocket Smoothie)
+
+streamlit.text('Hard-boiled & Free-range egg')
