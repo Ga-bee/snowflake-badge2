cc79b23b47679333c1af71cafcf8add38481e437
Ga-bee <116361973+Ga-bee@users.noreply.github.com>
2022-12-13 08:02:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,12 @@ streamlit.title('My parents new healthy diner')
 
 strealit.header('Breakfest menu')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 
-streamlit.text('Kale, Spinach & Rocket Smoothie)
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie)
 
-streamlit.text('Hard-boiled & Free-range egg')
+streamlit.text('🐔Hard-boiled & Free-range egg')
+ 
+streamlit.text('🥑🍞Avocado Toast')
+               
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
