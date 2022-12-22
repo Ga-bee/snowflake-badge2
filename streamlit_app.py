a52679ba49149834b3f23558ab6d8ee1375197fd
Ga-bee <116361973+Ga-bee@users.noreply.github.com>
2022-12-22 12:29:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,11 +27,9 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.dataframe(my_fruit_list)
 
-# Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
@@ -52,17 +50,27 @@ except URLError as e:
  streamlit.error()
 
 
+streamlit.header("The fruit load list contains:")
+def get_fruit_load_list():
+ with my_cnx.cursor as my_cur:
+  my_cur.execute("SELECT * from fruit_load_list")
+  return my_cur.fetchall()
+ 
+ if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_loat_list()
+  streamlit.dataframe(my_data_rows)
+  
+
 streamlit.stop()
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
+def insert_row_snowflake(new_fruit):
+ with my_cnx.cursor() as my_cur:
+  my_cur.execute(f"insert into fruit_load_list values ({add_my_fuit})")
+  return 'Thanks for adding ' + new_fruit
+  
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-my_cur.execute(f"insert into fruit_load_list values ({add_my_fuit})")
-my_data_rows = my_cur.fetchall()
-streamlit.text(f'Thanks for adding "{add_my_fruit}" ')
+if streamlit.button('Add a fruit to the list'):
+ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]
+ back_from_function = insert_row_snowflake(add_my_fruit)
+ streamlit.text(back_from_fuction)
